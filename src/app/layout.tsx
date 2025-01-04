8bd7160f3398adeb884c7ebb5330a7a211e868a3
Kevin Hou <strawberrypi64@gmail.com>
2025-01-04 14:57:37 +1100

    feat: add json-ld

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,44 @@
 import "./globals.css";
 
+import { site } from "@/config/site";
+
+import Script from "next/script";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" dir="ltr" suppressHydrationWarning>
+      <head>
+        <Script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": ["ProfilePage"],
+              dateCreated: "2025-01-01T00:00:00+11:00",
+              dateModified: new Date(),
+              name: site.name.default,
+              url: site.links.website,
+              description: site.description,
+              creator: {
+                "@type": "Person",
+                name: site.name.short,
+                url: site.links.website,
+              },
+              keywords: site.keywords,
+              inLanguage: "en-AU",
+              mainEntity: {
+                "@type": "Person",
+                name: "Kevin Hou",
+                alternateName: "kevinnhou",
+              },
+            }),
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
