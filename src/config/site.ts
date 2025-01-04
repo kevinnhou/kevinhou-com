985b4b72a0c2996662380cd4b3556abbe4aaee4e
Kevin Hou <strawberrypi64@gmail.com>
2025-01-04 14:53:38 +1100

    chore: add site config

---
+++ src/config/site.ts
@@ -0,0 +1,15 @@
+export const site = {
+  name: {
+    default: "Kevin Hou - Student",
+    short: "Kevin Hou",
+  },
+  description: "The Portfolio of Kevin Hou, Student",
+  keywords: ["Kevin Hou"],
+  links: {
+    website: "https://kevinhou.com",
+    twitter: "https://twitter.com/kevinnhou",
+    github: "https://github.com/kevinnhou",
+    linkedin: "https://www.linkedin.com/in/kevinnhou",
+  },
+  tag: "kevinnhou",
+};
