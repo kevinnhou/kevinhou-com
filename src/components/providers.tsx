b36b4aa0400727617bfca486f38923634fed149d
Kevin Hou <strawberrypi64@gmail.com>
2025-03-22 23:33:39 +1100

    chore: initialise repository

---
+++ src/components/providers.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import type { ReactNode } from "react"
+
+import { ThemeProvider, useTheme } from "next-themes"
+import { Toaster } from "sonner"
+
+export default function Providers({ children }: { children: ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      disableTransitionOnChange
+      enableSystem
+    >
+      <ToasterProvider />
+      {children}
+    </ThemeProvider>
+  )
+}
+
+function ToasterProvider() {
+  const { theme } = useTheme() as {
+    theme: "dark" | "light" | "system"
+  }
+  return (
+    <Toaster
+      position="bottom-center"
+      richColors
+      theme={theme}
+    />
+  )
+}
